6b18eaf44998761ab876f866a55b0051da3a796c
Yossi Rizgan <yosriz@gmail.com>
2018-07-16 14:48:19 +0300

    question item ui

--- src/QuestionListItem.js
+++ src/QuestionListItem.js
@@ -21,28 +21,48 @@ export default class QuestionListItem extends PureComponent<Props>{
     render() {
         const question = this.props.question;
         return (
-            <View style={styles.vertical}>
-            <View style={styles.vertical}>
-                <View style={{width: '10%', alignSelf:'center', alignItems:'center'}}>
-                    <Text style={{ color: colors.primary }}>
-                        {question.up_vote_count}</Text>
+            <View>
+                <View style={styles.horizontal}>
+                    <View style={{ width: '15%', alignSelf: 'center', alignItems: 'center' }}>
+                        <View style={{
+                            borderWidth: 1,
+                            borderColor: '#adadad',
+                            borderRadius: 3,
+                            width: '60%'
+                        }}>
+                            <Text style={{ textAlign: 'center' }}>
+                                {question.up_vote_count}</Text>
+                        </View>
+                    </View>
+                    <Text style={{ marginLeft: 5, marginBottom: 2, marginTop: 2, color: colors.primary, width: '90%' }}>{question.title}</Text>
                 </View>
-                <Text style={{ margin: 5, color: colors.primary, width: '90%' }}>{question.title}</Text>
-            </View>
-               {/* <View style={styles.horizontal}>
-                   <Text>3223</Text>
-                   <Text>{question.tags}</Text>
-               </View> */}
-           </View> 
+                <View style={styles.horizontal}>
+                    <View style={{ width: '15%', alignSelf: 'center', alignItems: 'center', flexDirection: 'row' }}>
+                        <Text style={{ color: colors.primary, marginLeft : 10, marginRight : 2 }}>
+                            323</Text>
+                        <Image style={{ width : 12, height : 12}}
+                            source={require('../kin-icon.png')}
+                        />
+                    </View>
+                    <Text style={{ marginLeft: 5, marginBottom: 2, marginTop: 2, color: "#adadad", flexGrow: 1 }}>
+                        {question.tags.reduce((prevVal, currVal, index, array) => {
+                            return prevVal + ', ' + currVal
+                        })}
+                    </Text>
+                    <Text style={{ marginRight: 5, marginBottom: 2, marginTop: 2, color: "#adadad" }}>
+                        22 Hours Ago
+                    </Text>
+                </View>
+            </View >
         );
     }
 }
 
 const styles = StyleSheet.create({
-    horizontal: {
+    vertical: {
         flexDirection: 'column',
     },
-    vertical: {
+    horizontal: {
         flexDirection: 'row',
     },
 

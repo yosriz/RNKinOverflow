f22af2da11332fef0666aabc1d4adc1beec161cc
Yossi Rizgan <yosriz@gmail.com>
2018-08-04 23:41:07 +0300

    tabs with title bar

--- App.js
+++ App.js
@@ -6,92 +6,55 @@
 
 import React, { Component } from 'react';
 import {
+  Image,
   Button,
   Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createStackNavigator, TabNavigator } from 'react-navigation';
 import { QuestionsScreen } from './src/screens/QuestionsScreen';
 import { ProfileScreen } from './src/screens/ProfileScreen';
 import { colors } from './src/Colors';
 
-const RootStack = createBottomTabNavigator({
-  Questions: QuestionsScreen,
+export const RootTabs = createBottomTabNavigator({
+  Questions: {
+    screen: QuestionsScreen, navigationOptions: ({ navigation }) => {
+    }
+  },
   Profile: ProfileScreen
 },
   {
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      return (<Image style={[
+        { width: 25, height: 25 },
+      ]}
+        source={require('./questions_icon.png')} />);
+    }
+  }
+);
+
+export const RootStack = createStackNavigator({
+  Root: {
+    screen: RootTabs,
     navigationOptions: ({ navigation }) => ({
+      title: 'KinOverflow',
       headerStyle: {
-        backgroundColor: '#f4511e',
+        backgroundColor: colors.primary,
       },
       headerTintColor: '#fff',
       headerTitleStyle: {
         fontWeight: 'bold',
-      },
-      tabBarIcon: ({ focused, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === 'Home') {
-          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-        } else if (routeName === 'Settings') {
-          iconName = `ios-options${focused ? '' : '-outline'}`;
-        }
-
-        return (<Image />);
-      },
-    }),
-    tabBarOptions: {
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-    },
+      }
+    })
   }
-);
-
-
+})
 
 export default class App extends React.Component {
   render() {
     return <RootStack />;
   }
 }
-
-
-
-// type Props = {};
-// export default class App extends Component<Props> {
-//   render() {
-//     const user = { title: "Yossi Rizgan", icon: "https://media.licdn.com/dms/image/C4D03AQFIKDRr1Hegew/profile-displayphoto-shrink_200_200/0?e=1534377600&v=beta&t=Vp2c20_6fqQMIsH1aZsVJcCZFrKD5qOUP7RYBKOPBvY" };
-//     const question = {
-//       title: "Some awesome question about some interesting answers that do not fit in one line",
-//       up_vote_count: 4, tags: ["javascript", "react-native"]
-//     };
-//     return (
-//       <View >        
-//         <QuestionListItem question={question} />
-//         <QuestionListItem question={question} />
-//         <QuestionListItem question={question} />
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
